e76c87f8da85c68a389d5e414abaa72303584e27
Abda Shaffan <abdashaffan@gmail.com>
2019-05-25 10:05:20 +0700

    Implement request method

--- src/scrap.py
+++ src/scrap.py
@@ -6,25 +6,50 @@ from word2number import w2n
 
 # Modul untuk get request ke URL
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 # Modul buat ngolah data JSON
 import json
 
 # Modul tidur
-from time import sleep
+import time
+
+
+def requests_retry_session(
+    retries=3, backoff_factor=0.3, status_forcelist=(500, 502, 504), session=None
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+    return session
+
 
 page_num = 1
 book_list = []
-book_num = 0
 book_id = 1000
 
 while page_num <= 50:
+    t0 = time.time()
     try:
         # Ambil semua isi HTML dari books.toscrape.com
         url = "http://books.toscrape.com/catalogue/page-{}.html".format(page_num)
-        source = requests.get(url).text
-        soup = BeautifulSoup(source, "lxml")
-
+        # source = requests.get(url, timeout=None).text
+        response = requests_retry_session().get(url)
+        # print("page {} status: {}".format(page_num, response.status_code))
+    except Exception as e:
+        print("{} Connection failed: {}".format(url, e.__class__.__name__))
+    else:
+        print("{} connected. Status: {}".format(url, response.status_code))
+        soup = BeautifulSoup(response.text, "lxml")
         # Mencari data-data yang dibutuhkan dari container div "product_pod"
         books = soup.find_all("article", class_="product_pod")
         for book in books:
@@ -46,26 +71,30 @@ while page_num <= 50:
             book_dict["link"] = book_link
             # Masukkan dict ke dalam list semua buku pada katalog
             book_list.append(book_dict)
-            # Tracker (supaya gak bosen nunggunya)
-            book_num += 1
-            print("books loaded: {}".format(book_num))
-
         page_num += 1
         # Supaya gak limit requestnya
-        sleep(0.5)
+        # sleep(2)
+    finally:
+        t1 = time.time()
+        print("took {} seconds\n\n".format(t1 - t0))
 
-    except Exception as e:
-        # print(e.__class__.__name__)
-        print(e)
-        break
 
 for book in book_list:
     # Loop untuk mengambil detail tambahan dari setiap buku yang sudah diambil linknya
+    t0 = time.time()
     try:
         # Ambil semua isi HTML dari buku
         url = book["link"]
-        source = requests.get(url).text
-        soup = BeautifulSoup(source, "lxml")
+        # source = requests.get(url, timeout=None).text
+        response = requests_retry_session().get(url)
+        # print("link {} status: {}".format(url, response.status_code))
+
+    except Exception as e:
+        print("{} Connection failed: {}".format(url, e.__class__.__name__))
+
+    else:
+        print("{} connected. Status: {}".format(url, response.status_code))
+        soup = BeautifulSoup(response.text, "lxml")
         # Ambil kategori buku dari ul dengan class breadcrumb
         breadcrumb = soup.find("ul", class_="breadcrumb")
         breadcrumb_list = breadcrumb.find_all("li")
@@ -80,28 +109,25 @@ for book in book_list:
         book_stock = int("".join(filter(str.isdigit, instock)))
         book["stock"] = book_stock
         # Ambil summary dari buku
-        summary_el = soup.find("div", id="product_description").findNext("p")
+        summary_el = soup.find("div", id="product_description")
         if summary_el is None:
             summary = "-"
         else:
+            summary_el = summary_el.findNext("p")
             summary = summary_el.text.split("...")[0].strip()
-
         book["summary"] = summary
         # Ambil kode UPC buku
         upc = soup.find("tr").td.text
         book["upc"] = upc
-
-        print(json.dumps(book, sort_keys=True, indent=3))
+        # print(json.dumps(book, sort_keys=True, indent=3))
         # Supaya gak limit requestnya
-        sleep(2)
+        # sleep(2)
 
-    except Exception as e:
-        # print(e.__class__.__name__)
-        print(e)
-        break
+        # Menyimpan dict buku ke dalam file "books.json"
+        filename = "books.json"
+        with open(filename, "w") as f:
+            json.dump(book_list, f, sort_keys=True, indent=4)
 
-
-# Menyimpan dict buku ke dalam file "books.json"
-filename = "books.json"
-with open(filename, "w") as f:
-    json.dump(book_list, f, sort_keys=True, indent=4)
+    finally:
+        t1 = time.time()
+        print("took {} seconds\n\n".format(t1 - t0))
