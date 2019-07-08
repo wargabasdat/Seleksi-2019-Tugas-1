cb574a6c728a24bdccc62a9606d7d5421b051dac
Mahanti Indah <mahantiindah@gmail.com>
2019-07-08 14:37:04 +0700

    final huhu

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ def ambil_data(url):
         jam = umur.findNext("td", text = "Jam Bertamu").findNextSibling().findNextSibling()
         tipe_kos["Jam Bertamu"] = jam.text.strip()
     else:
-        tipe_kos["Umur Bangunan"] = None
+        tipe_kos["Umur Bangunan"] = "-"
         jam = jenis.findNext("td", text = "Jam Bertamu").findNextSibling().findNextSibling()
         tipe_kos["Jam Bertamu"] = jam.text.strip()
     pelihara = jam.findNext("td", text = "Pelihara Binatang").findNextSibling().findNextSibling()
@@ -47,57 +47,56 @@ def ambil_data(url):
     data["Tipe Kos"] = tipe_kos
 
     jumlah = soup.find("div", {"class" : "dataListing dataRight"})
-    data["Jumlah Kamar"] = int(jumlah.text.strip())
+    data["Jumlah Kamar"] = int(re.findall("\d+", jumlah.text.strip())[0])
     ukuran = jumlah.findNext("div", {"class" : "dataListing dataRight"})
     data["Ukuran Kamar"] = ukuran.text.strip()
     luas = ukuran.findNext("div", {"class" : "dataListing dataRight"})
     data["Luas Kamar"] = int(re.findall("\d+", luas.text.strip())[0])
     
     fk = {}
-    list_fk = ["AC", "Internet/ Wifi", "TV Kabel", "Lemari Pakaian", "Kamar Mandi Dalam", "Water Heater", "Spring Bed", "Meja Belajar", "Kursi Belajar", "TV", "Kulkas", "Kasur", "Tempat Jemuran", "Teras", "Kamar Kosongan", "Laundry", "Sekamar Berdua", "Sekamar Bertiga"]
+    list_fk = ["AC", "Internet/ Wifi", "TV", "Lemari Pakaian", "Kamar Mandi Dalam", "Water Heater", "Kasur", "Meja Belajar", "Kursi Belajar", "Kulkas"]
     for fasilitas in list_fk:
         fk[fasilitas] = "tidak"
     fasilitas_kamar = soup.findAll("div", {"class" : "row text-left"})
     for fasilitas in fasilitas_kamar:
-        fk[fasilitas.text.strip()] = "ya"
+        if fasilitas.text.strip() in list_fk:
+            fk[fasilitas.text.strip()] = "ya"
     data["Fasilitas Kamar"] = fk
 
     fb = {}
-    list_fb = ["Ruang Tamu", "Ruang Makan", "Keamanan 24 Jam", "Parkir Mobil", "Parkir Motor", "Dapur", "Pembantu", "Mesin Cuci", "Kulkas", "Dispenser Air Minum", "Ruang Keluarga", "TV"]
+    list_fb = ["Ruang Tamu", "Ruang Makan", "Keamanan 24 Jam", "Parkir Mobil", "Parkir Motor", "Dapur", "Pembantu", "Mesin Cuci"]
     for fasilitas in list_fb:
         fb[fasilitas] = "tidak"
     fasilitas_bangunan = soup.find("h3", text = "Fasilitas Bangunan")
     fbs = fasilitas_bangunan.find_next_siblings("div", {"class" : "col-md-3 col-xs-6 fasilitas"})
     for fasilitas in fbs:
-        fb[fasilitas.p.text.strip()] = "ya"
-        time.sleep(1)
-
+        if fasilitas.text.strip() in list_fb:
+            fb[fasilitas.p.text.strip()] = "ya"
     data["Fasilitas Bangunan"] = fb
     
     fs = {}
-    list_fs = ["ATM", "Mini Market", "Tempat Ibadah", "Kampus", "Sekolah", "Game Online", "Warteg", "Restoran", "Keamanan 24 Jam", "Satpam", "Laundry Shop", "Angkot", "Ojek", "Rumah Sakit", "GYM", "Mall", "Pom Bensin", "Lapangan Bola", "Dekat Pintu Tol", "Puskesmas"]
+    list_fs = ["ATM", "Mini Market", "Tempat Ibadah", "Warteg", "Restoran", "Laundry Shop", "Rumah Sakit", "GYM", "Mall"]
     for fasilitas in list_fs:
         fs[fasilitas] = "tidak"
     fasilitas_sekitar = soup.find("h3", text = "Fasilitas Sekitar")
     fss = fasilitas_sekitar.find_next_siblings("div", {"class" : "col-md-3 col-xs-6 fasilitas"})
     for fasilitas in fss:
-        fs[fasilitas.p.text.strip()] = "ya"
-        time.sleep(1)
-
+        if fasilitas.text.strip() in list_fs:
+            fs[fasilitas.p.text.strip()] = "ya"
     data["Fasilitas Sekitar"] = fs
 
     return data
 
 if __name__ == "__main__":
     header = {"user-agent" : "mahantiindah@gmail.com"}
-    url = "https://www.cari-kos.com/search/kos/jawa-barat/kota-bandung?price-from=0&price-to=10000000"
+    url = "https://www.cari-kos.com/search/kos/dki-jakarta?price-from=0&price-to=10000000"
     response = requests.get(url, headers = header)
     soup = BeautifulSoup(response.text, "html.parser")
 
     hasil = []
     container = soup.find("div", {"id" : "listing-container"})
     details = container.find_all("a", href = True)
-    for i in range (5):
+    for i in range (1):
         print(details[i]["href"])
         hasil.append(ambil_data(details[i]["href"]))
         time.sleep(2)
